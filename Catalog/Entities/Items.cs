585cd77f44c775a25250a5223f448cc032d463c1
Mathew Harvey <mathewharvey@gmail.com>
2022-06-08 17:38:43 +0800

    routes added

--- Catalog/Entities/Items.cs
+++ Catalog/Entities/Items.cs
@@ -1,8 +1,14 @@
+using Catalog.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+using System;
+
 namespace Catalog.Entities
 {
         public record Item
         {
-            public Guid id { get; init; }
+            public Guid Id { get; init; }
             public string Name { get; init;}
             public decimal Price { get; init; }
             public DateTimeOffset CreatedDate { get; init; }
--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -1,12 +1,20 @@
 using Catalog.Entities;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using System.Collections.Generic;
+
+
 
 namespace Catalog.Repositories
 {
-    public class InMemItemsRepository
+   
+
+    public class InMemItemsRepository : IItemsRepository
     {
         private readonly List<Item> items = new()
         {
-            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id= Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
             new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
             new Item { Id = Guid.NewGuid(), Name = "Bronze Sheild", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
 
@@ -17,7 +25,7 @@ namespace Catalog.Repositories
             return items;
         }
 
-        public Item GetItem (Guid id)
+        public Item GetItem(Guid id)
         {
             return items.Where(item => item.Id == id).SingleOrDefault();
         }
